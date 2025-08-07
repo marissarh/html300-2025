e515d65fccd555f292233ce4352a20073b121fcb
Marissa <102936996+marissarh@users.noreply.github.com>
2025-08-06 20:07:19 -0700

    add directives to state_variables branch

--- 04-Intro_to_Vue/lesson-app/src/App.vue
+++ 04-Intro_to_Vue/lesson-app/src/App.vue
@@ -2,6 +2,12 @@
 import { ref } from "vue";
 
 const text = ref("Hello Coders!!");
+
+function changeText(event){
+  text.value = event.target.value;
+}
+
+const hyperlink = ref('https://vuejs.org/')
 </script>
 
 <template>
@@ -14,6 +20,13 @@ const text = ref("Hello Coders!!");
       height="125"
     />
     <h1>{{ text }}</h1>
+
+    <!-- v-on syntax example: -->
+     <input v-model="text" v-on:input="changeText"/>
+     <!--anchor tag with v-bind directive to bing a property-->
+      <a v-bind:href="hyperlink">{{ hyperlink }}</a>
+      
+    
   </header>
 </template>
 
