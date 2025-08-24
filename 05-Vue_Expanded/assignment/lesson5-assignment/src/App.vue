208f51ed00a8f24ef4ad1b5e44420a4f407cdfdd
Marissa <102936996+marissarh@users.noreply.github.com>
2025-08-24 16:29:17 -0700

    add new files

---
+++ 05-Vue_Expanded/assignment/lesson5-assignment/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HeaderPage>
+      <template>
+        <h1 class="text-primary">Welcome to the Images Page</h1>
+      </template>
+    </HeaderPage>
+    <ImagesPage />
+  </div>
+</template>
+
+<script>
+ import HeaderPage from './components/HeaderPage.vue';
+ import ImagesPage from './pages/ImagesPage.vue';
+export default {
+  components:{
+    HeaderPage,
+    ImagesPage
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 05-Vue_Expanded/assignment/lesson5-assignment/src/components/ImageComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <img 
+        :src="src" 
+        :alt="alt"
+        :title="title"
+        @click="toggleBorder"
+        class="['img-fluid', hasBorder ? 'border border-secondary' : '']"/>
+    </div>
+</template>
+<script>
+import ImageMixin from './ImageMixin';
+
+
+export default {
+    name: 'ImageComponent',
+    mixins: [ImageMixin],
+    props:{
+        src:{
+            type: String,
+            required: true},
+
+        alt: {
+            type: String,
+            required: true},
+        title: {
+            type: String,
+            default: ''
+        },
+    }
+}
+</script>
+<style></style>
